44229aadaf9c0f3f25f347238f25ceedee390078
yoursmukund <mukundsharma@outlook.com>
2014-06-09 07:00:57 +0530

    Saving changes with signin failure done till page 338

--- demo_app/app/controllers/sessions_controller.rb
+++ demo_app/app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   	@title = "sign in"
   end
   def create
+  	user = User.authenticate(params[:session][:email], params[:session][:password])
+  	if user.nil?
+  		flash.now[:error] = "Invalid Email Password Combination"
+  		@title = "sign in"
+  		render 'new'
+  	else
+  		#signin n render show page
+  	end
   end
   def destroy
   end
--- demo_app/spec/controllers/sessions_controller_spec.rb
+++ demo_app/spec/controllers/sessions_controller_spec.rb
@@ -13,5 +13,25 @@ render_views
     response.should have_selector("title", :content => "sign in")
   end
 end
-
+describe "POST 'Create" do
+	describe "invalid signin" do
+		before(:each) do
+			@attr = {:email => "email@example.com", :password => "invalid"}
+		end
+		it "should render a new page" do
+			post :create, :session => @attr
+			response.should render_template('new')
+		end
+		it "should have right title" do
+			post :create, :session => @attr
+			response.should have_selector("title", :content => "sign in")
+		end
+		it "should have a flash now message" do
+			post:create, :session => @attr
+			flash.now[:error].should =˜ /invalid/i
+		end
+		
+	end
+				
+end
 end
